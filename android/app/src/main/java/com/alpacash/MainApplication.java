2bb1bf7c4016c26d2f56c96521c76ec465c60da7
Jolyn Tan <41572120+jolynnn8D@users.noreply.github.com>
2019-05-21 23:50:27 +0800

    added firebase

--- android/app/src/main/java/com/alpacash/MainApplication.java
+++ android/app/src/main/java/com/alpacash/MainApplication.java
@@ -3,6 +3,7 @@ package com.alpacash;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
