8f1b39db08f598256d6a04aec4149bad5809fa25
Valary Lim <valarylim@outlook.com>
2019-06-22 15:33:29 +0800

    basic animations

--- app/containers/Budget.js
+++ app/containers/Budget.js
@@ -3,19 +3,78 @@ import {
     StyleSheet,
     View, 
     Text, 
-    TextInput, 
     ScrollView, 
-    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Animated,
+    Image,
+    Easing
 } from "react-native";
-/*
-    other import statements or 
-    JS variables like const here - can be dummy data to use for development
-*/
+
 export default class Budget extends Component {
+    constructor() {
+        super();
+        this.moveLeftValueHolder = new Animated.ValueXY({x: 200, y: 200});
+        this.flipValueHolder = new Animated.Value(0);
+    }
+
+    componentDidMount() {
+        this._moveLeftAnimation();
+    }
+
+    _moveLeftAnimation() {
+        Animated.timing(this.moveLeftValueHolder,
+        {
+            toValue: {x: 1, y: 10},
+            duration: 10000,
+            easing: Easing.linear
+        }).start(()=>this._flipAnimation())
+    }
+
+    _flipAnimation() {
+        if (this.flipValueHolder >= 90) {
+            Animated.spring(
+                this.flipValueHolder,
+                {
+                    toValue: 0,
+                    tension: 10,
+                    friction: 8
+                }
+            ).start();
+        } else {
+            Animated.spring(
+                this.flipValueHolder,
+                {
+                    toValue: 180,
+                    tension: 10,
+                    friction: 8
+                }
+            ).start();
+        }
+    }
+    
     render() {
+        this.setFlipInterpolate = this.flipValueHolder.interpolate({
+            inputRange: [0, 180],
+            outputRange: ['180deg', '360deg']
+        })
+
         return(
-            <View>
-                <Text>Budget</Text>
+            <View style = {styles.container} >
+                <Animated.Image
+                    source = {require('../assets/images/alpacas/alpaca1.png')}
+                    style = {{ 
+                        width: 50, 
+                        height: 80,
+                        transform: [{ rotateY: this.setFlipInterpolate }]}}
+                />
+                <Animated.Image
+                    source = {require('../assets/images/alpacas/alpaca2.png')}
+                    style = {{ 
+                        width: 50, 
+                        height: 75,
+                        transform: this.moveLeftValueHolder.getTranslateTransform()
+                        }}
+                />
             </View>
         );
     }
