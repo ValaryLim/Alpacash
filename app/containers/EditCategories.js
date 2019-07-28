482f35385cdd3e0554c770ce2cad2512f49801eb
Jolyn Tan <41572120+jolynnn8D@users.noreply.github.com>
2019-07-28 11:53:31 +0800

    Allow budget to refresh weekly (#23)

--- app/containers/EditCategories.js
+++ app/containers/EditCategories.js
@@ -3,15 +3,103 @@ import React, { Component } from "react";
 import { 
     StyleSheet,
     View, 
+    ScrollView,
     Text
 } from "react-native";
 
-export default class Achievements extends Component {
+
+
+import ScrollableTabView, {DefaultTabBar, ScrollableTabBar} from 'react-native-scrollable-tab-view-forked'
+import firebase from 'react-native-firebase';
+
+
+export default class EditCategories extends Component {
+    constructor() {
+        super();
+        this.expense_categories = firebase.firestore().collection('expense_categories');
+        this.income_categories = firebase.firestore().collection('income_categories');
+        this.unsubscribe_expense_categories = null;
+        this.unsubscribe_income_categories = null;
+        this.state = {
+            loading: true,
+            expenseCategories: [],
+            incomeCategories: [],
+        }
+    }
+
+    componentDidMount() {
+        this.unsubscribe_expense_categories = this.expense_categories.onSnapshot(this.onExCategoriesUpdate);
+        this.unsubscribe_income_categories = this.income_categories.onSnapshot(this.onInCategoriesUpdate);
+    }
+    
+    componentWillUnmount() {
+        this.unsubscribe_expense_categories();
+        this.unsubscribe_income_categories();
+    }
+
+    onInCategoriesUpdate = (querySnapshot) => {
+        const incomeCategories = [];
+        querySnapshot.forEach((doc) => {
+          const { id, title, checked, color } = doc.data();
+          
+          incomeCategories.push({
+            key: doc.id,
+            doc, // DocumentSnapshot
+            id,
+            title,
+            checked,
+            color
+          });
+        });
+        this.setState({ 
+          incomeCategories,
+          loading: false
+       });
+    
+    }
+
+    onExCategoriesUpdate = (querySnapshot) => {
+        const expenseCategories = [];
+        querySnapshot.forEach((doc) => {
+          const { id, title, checked, color } = doc.data();
+          
+          expenseCategories.push({
+            key: doc.id,
+            doc, // DocumentSnapshot
+            id,
+            title,
+            checked,
+            color
+          });
+        });
+        this.setState({ 
+          expenseCategories,
+          loading: false
+       });
+     }
+
     render() {
-        return(
-            <View style = {styles.main}>
-                <Text>Edit Categories Screen</Text>
-            </View>
+        return(        
+            <ScrollableTabView
+
+                renderTabBar={() => (
+                <ScrollableTabBar
+                    style={styles.scrollStyle}
+                    tabStyle={styles.tabStyle}
+                />
+                )}
+                tabBarTextStyle={styles.tabBarTextStyle}
+                tabBarInactiveTextColor={'black'}
+                tabBarActiveTextColor={'red'}
+                tabBarUnderlineStyle={styles.underlineStyle}
+                initialPage={2}
+                >
+
+            <View key={'1'} tabLabel={'Expenditure'} style={{flex:1,backgroundColor:'white'}}/>   
+            <View key={'2'} tabLabel={'  Income'} style={{flex:1,backgroundColor:'white'}}/>
+
+            </ScrollableTabView>
+
         );
     }
 }
