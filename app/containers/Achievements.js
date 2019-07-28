e0a2bf1a33a6b1015d5a41a0f49f6af4e0dc3150
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-07-28 18:47:43 +0800

    Update alpaca achievement page based on budget reset

--- app/containers/Achievements.js
+++ app/containers/Achievements.js
@@ -3,117 +3,46 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 
 export default class Achievements extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [
-                { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                { name: "Level 2", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca2_black.png") }, 
-                { name: "Level 3", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca3_black.png") }, 
-                { name: "Level 4", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca4_black.png") }, 
-                { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
-                { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-            ]
+    static items = [
+        { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
+        { name: "Level 2", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca2_black.png") }, 
+        { name: "Level 3", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca3_black.png") }, 
+        { name: "Level 4", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca4_black.png") }, 
+        { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
+        { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
+    ]
+
+    static achievedLevel(levelNum) {
+        const itemsUpdate = this.items;
+
+        // Update colour
+        itemsUpdate[levelNum - 1].color = "#BBDED6";
+        
+        // Update image source
+        if (levelNum === 1) {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca1_achieved.png");
+        } else if (levelNum === 2) {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca2_achieved.png");   
+        } else if (levelNum === 3) {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca3_achieved.png");
+        } else if (levelNum === 4) {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca4_achieved.png");
+        } else if (levelNum === 5) {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca5_achieved.png");
+        } else {
+            itemsUpdate[levelNum - 1].source = require("../assets/images/achievements/achieved/alpaca6_achieved.png");
         }
+        
+        this.items = itemsUpdate;
     }
-
-    achievedLevel1() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca2_black.png") }, 
-                    { name: "Level 3", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca3_black.png") }, 
-                    { name: "Level 4", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca4_black.png") }, 
-                    { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
-                    { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-                ]
-            }
-        );
-    }
-
-    achievedLevel2() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca2_achieved.png") }, 
-                    { name: "Level 3", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca3_black.png") }, 
-                    { name: "Level 4", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca4_black.png") }, 
-                    { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
-                    { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-                ]
-            }
-        );
-    }
-
-    achievedLevel3() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca2_achieved.png") }, 
-                    { name: "Level 3", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca3_achieved.png") }, 
-                    { name: "Level 4", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca4_black.png") }, 
-                    { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
-                    { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-                ]
-            }
-        );
-    }
-
-    achievedLevel4() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca2_achieved.png") }, 
-                    { name: "Level 3", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca3_achieved.png") }, 
-                    { name: "Level 4", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca4_achieved.png") }, 
-                    { name: "Level 5", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca5_black.png") }, 
-                    { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-                ]
-            }
-        );
-    }
-
-    achievedLevel5() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca2_achieved.png") }, 
-                    { name: "Level 3", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca3_achieved.png") }, 
-                    { name: "Level 4", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca4_achieved.png") }, 
-                    { name: "Level 5", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca5_achieved.png") }, 
-                    { name: "Level 6", color: "#FFB6B9", source: require("../assets/images/achievements/unachieved/alpaca6_black.png") },
-                ]
-            }
-        );
-    }
-
-    achievedLevel6() {
-        this.setState(
-            {
-                items: [
-                    { name: "Level 1", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca1_achieved.png") }, 
-                    { name: "Level 2", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca2_achieved.png") }, 
-                    { name: "Level 3", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca3_achieved.png") }, 
-                    { name: "Level 4", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca4_achieved.png") }, 
-                    { name: "Level 5", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca5_achieved.png") }, 
-                    { name: "Level 6", color: "#BBDED6", source: require("../assets/images/achievements/achieved/alpaca6_achieved.png") },
-                ]
-            }
-        );
-    }
-
+    
     render() {
         return (
             <View style = {styles.main }>
                 <Text style = { styles.header }>ACHIEVEMENTS</Text>
                 <FlatGrid
                     itemDimension={ 130 }
-                    items={ this.state.items }
+                    items={ Achievements.items }
                     style={ styles.gridView }
                     renderItem={({ item, index }) => (
                     <View style={ styles.itemContainer }>
