121b5fb9c029c486be28a3af5d7469c80f343c64
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-07-28 11:52:15 +0800

    Add alert on event end

--- app/components/MovableAlpaca.js
+++ app/components/MovableAlpaca.js
@@ -13,17 +13,42 @@ import {
 
 import Gestures from 'react-native-easy-gestures';
 
+import {createResponder} from "react-native-gesture-responder";
+
 import Alpaca from "./Alpaca";
 
 export default class MovableAlpaca extends Component {
+    componentWillMount() {
+        this.gestureResponder = createResponder({
+            onStartShouldSetResponder: (evt, gestureState) => true,
+            onStartShouldSetResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetResponder: (evt, gestureState) => true,
+            onMoveShouldSetResponderCapture: (evt, gestureState) => true,
+            onResponderGrant: (evt, gestureState) => {},
+            onResponderMove: (evt, gestureState) => {},
+            onResponderTerminationRequest: (evt, gestureState) => true,
+            onResponderRelease: (evt, gestureState) => {},
+            onResponderTerminate: (evt, gestureState) => {},
+            
+            onResponderSingleTapConfirmed: (evt, gestureState) => {},
+            
+            moveThreshold: 2,
+            debug: false
+        });
+    }
     render() {
         return (
             <View>
                 <Gestures
                     rotatable = {false}
                     scalable = {false} 
+                    onEnd={(event, styles) => {
+                        alert(this.myComponent.measure((fx, fy, width, height, px, py) => {
+                            alert(py);
+                        }));
+                    }}    
                 >
-                    <Alpaca/>
+                    <Alpaca style = {{ overflow: "hidden" }} />
                 </Gestures>
             </View>
         );
